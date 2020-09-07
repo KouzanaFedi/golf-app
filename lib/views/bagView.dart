9d6f573806976c92feefe74b3354b3341bc08ef1
kouzana fedi <fedi.kouzana1@gmail.com>
2020-09-07 23:29:33 +0100

    Init flutter project

---
+++ lib/views/bagView.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BagView extends StatefulWidget {
+  @override
+  _BagViewState createState() => _BagViewState();
+}
+
+class _BagViewState extends State<BagView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
